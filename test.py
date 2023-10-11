5499be35a5631a606c56f4c081a08a0ae5cb6a4b
Muhammad Abdullah <sipra58@gmail.com>
2023-10-11 14:12:54 +0500

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import base64
 
 Base_url="http://127.0.0.1:5000/"
 
-data={"email":"demo@gmail.com","password":"demo@1234"}
+data={"email":"demo@gmail.com","password":"demo@123"}
 
 response=requests.get(Base_url+"Login",data=data)
 print(response.json())
